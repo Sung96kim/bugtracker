0499b37f043e699e8630e7e7ebc1306ce1df8a16
Sung <sung96kim@gmail.com>
2021-01-20 16:10:16 -0500

    ADD login page

---
+++ src/Containers/LoginPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AppContainer from "./AppContainer";
+
+class LoginPage extends Component{
+    render(){
+        return(
+            <AppContainer>
+                <div> 
+
+                </div>
+            </AppContainer>    
+        );
+    }
+}
+
+export default LoginPage;
