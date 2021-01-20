612badd8dee85adfe8640e0754d750de3ad197d3
Sung <sung96kim@gmail.com>
2021-01-20 02:24:55 -0500

    navbar

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import HomePage from './HomePage';
+import HomePage from './Containers/HomePage';
 
 test('renders learn react link', () => {
   render(<HomePage />);
